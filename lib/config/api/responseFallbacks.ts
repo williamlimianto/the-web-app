99ad655160cd36b8eb4144083054003f79409b58
William Limianto <william.limianto@gmail.com>
2024-05-12 01:02:17 +0700

    feat: add fetch common helper

---
+++ lib/config/api/responseFallbacks.ts
@@ -0,0 +1,10 @@
+export const API_RESPONSE_FALLBACK = {
+  SUCCESS: {
+    status: 200,
+    message: 'Success',
+  },
+  INTERNAL_SERVER_ERROR: {
+    status: 500,
+    message: 'Internal Server Error',
+  },
+};
