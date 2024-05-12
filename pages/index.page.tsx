cf79e2e9f489d108131bde1547285c34b60cbd2b
William Limianto <william.limianto@gmail.com>
2024-05-12 21:25:30 +0700

    feat: add API response data intergation to the respective Dropdown Inputs

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,38 +1,87 @@
+import { useState } from 'react';
+import DropdownInput from '@components/Form/DropdownInput';
 import { useCountryListQuery, useStateListQuery } from '@lib/hooks/location';
+import {
+  INFO_REMARK_TEXT_LOADING,
+  PLACEHOLDER_TEXT_DROPDOWN_INPUT_COUNTRY_LIST,
+  PLACEHOLDER_TEXT_DROPDOWN_INPUT_STATE_LIST,
+} from './constants';
+import styles from './index.module.css';
 
 const LandingPage = () => {
+  const [selectedCountryId, setSelectedCountryId] = useState<number>(0);
+  const [selectedStateId, setSelectedStateId] = useState<number>(0);
   const {
-    isError: isFetchCountryListError,
+    isLoading: isFetchCountryListLoading,
     data: countryListData,
     error: countryListFetchErrorObj,
   } = useCountryListQuery();
-
   const {
-    isError: isFetchStateListError,
+    isLoading: isFetchStateListLoading,
     data: stateListData,
     error: stateListFetchErrorObj,
-  } = useStateListQuery(10);
+  } = useStateListQuery(selectedCountryId);
 
-  // [TODO]: UI Improvements - Will use dropdown UI for both cases on the separate PR.
+  const handleDropdownInputCountryChange = ({
+    selectedValue,
+  }: {
+    selectedValue: number;
+  }) => {
+    setSelectedCountryId(selectedValue);
+    setSelectedStateId(0);
+  };
+
+  const handleDropdownInputStateChange = ({
+    selectedValue,
+  }: {
+    selectedValue: number;
+  }) => {
+    setSelectedStateId(selectedValue);
+  };
+
   return (
     <>
-      <h1>Country List Data:</h1>
-      <pre data-testid="dummy-pre-country">
-        {JSON.stringify(
-          isFetchCountryListError
-            ? (countryListFetchErrorObj as Error)?.toString()
-            : countryListData || []
-        )}
-      </pre>
+      <div className={styles.dropdown_input_wrapper}>
+        <DropdownInput
+          rootClassname={styles.dropdown_input}
+          placeholderText={PLACEHOLDER_TEXT_DROPDOWN_INPUT_COUNTRY_LIST}
+          infoRemarkText={
+            isFetchCountryListLoading ? INFO_REMARK_TEXT_LOADING : ''
+          }
+          errorRemarkText={(countryListFetchErrorObj as Error)?.toString()}
+          disabled={
+            isFetchCountryListLoading || !(countryListData || []).length
+          }
+          value={selectedCountryId}
+          options={(countryListData || []).map((itemObj) => {
+            return {
+              value: itemObj?.id,
+              label: itemObj?.value,
+            };
+          })}
+          onChange={handleDropdownInputCountryChange}
+        ></DropdownInput>
+      </div>
 
-      <h1>State List Data:</h1>
-      <pre data-testid="dummy-pre-state">
-        {JSON.stringify(
-          isFetchStateListError
-            ? (stateListFetchErrorObj as Error)?.toString()
-            : stateListData || []
-        )}
-      </pre>
+      <div className={styles.dropdown_input_wrapper}>
+        <DropdownInput
+          rootClassname={styles.dropdown_input}
+          placeholderText={PLACEHOLDER_TEXT_DROPDOWN_INPUT_STATE_LIST}
+          infoRemarkText={
+            isFetchStateListLoading ? INFO_REMARK_TEXT_LOADING : ''
+          }
+          errorRemarkText={(stateListFetchErrorObj as Error)?.toString()}
+          disabled={isFetchStateListLoading || !(stateListData || []).length}
+          value={selectedStateId}
+          options={(stateListData || []).map((itemObj) => {
+            return {
+              value: itemObj?.id,
+              label: itemObj?.value,
+            };
+          })}
+          onChange={handleDropdownInputStateChange}
+        ></DropdownInput>
+      </div>
     </>
   );
 };
